d341b8f77e91cfa5d380cf209203f732871bebfd
Vannesa 文 <95283773+Xvannn07@users.noreply.github.com>
2025-04-25 18:57:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,42 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://xvannn.xyz'),
   title: 'Ekspann',
   description: 'My Personal Portfolio',
-  generator: 'Xva',
   openGraph: {
-    images: 'public/img/xvann_logo.png',
+    type: 'website',
+    url: 'https://xvannn.xyz',
+    title: 'Ekspann',
+    description: 'My Personal Portfolio',
+    images: [
+      {
+        url: 'https://xvannn.xyz/img/xvann_logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Logo Ekspann',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ekspann',
+    description: 'My Personal Portfolio',
+    images: ['https://xvannn.xyz/img/xvann_logo.png'],
   },
   icons: {
     icon: '/img/xvann_logo.png',
-  }
+  },
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
